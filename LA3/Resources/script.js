db36e33ac25a3dd99e77c3b78a094927613c46c7
Vinayak <vinayaknautiyal38@gmail.com>
2026-03-30 11:03:08 +0530

    updated la3

--- LA3/Resources/script.js
+++ LA3/Resources/script.js
@@ -1,10 +1,14 @@
+// Function to calculate Simple Interest
 function calculateSI() {
 
+    // Get input values and convert them to numbers
     var p = parseFloat(document.getElementById("p").value);
     var r = parseFloat(document.getElementById("r").value);
     var t = parseFloat(document.getElementById("t").value);
 
+    // Apply Simple Interest formula
     var si = (p * r * t) / 100;
 
+    // Display result on webpage
     document.getElementById("result").innerHTML = "Simple Interest: " + si;
 }
